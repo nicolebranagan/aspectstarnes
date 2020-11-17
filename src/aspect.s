6cec2616ccf5e9ae3ca746677dfdd1fa6cfb5f28
Nicole Branagan <nicolecarolinebranagan@gmail.com>
2020-11-17 17:52:56 -0500

    Remove empty 16KB PRG-ROM bank

--- src/aspect.s
+++ src/aspect.s
@@ -34,7 +34,7 @@ INES_MIRROR = 0 ; 0 = horizontal mirroring, 1 = vertical mirroring
 INES_SRAM   = 0 ; 1 = battery backed SRAM at $6000-7FFF
 
 .byte 'N', 'E', 'S', $1A ; ID
-.byte $02 ; 16k PRG chunk count
+.byte $01 ; 16k PRG chunk count
 .byte $01 ; 8k CHR chunk count
 .byte INES_MIRROR | (INES_SRAM << 1) | ((INES_MAPPER & $f) << 4)
 .byte (INES_MAPPER & %11110000)
